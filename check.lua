dc32611d678b1fff860fb3ecf512ea323b803238
idnohw <103338154+idnohw@users.noreply.github.com>
2024-09-21 22:42:24 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,1 +1,5 @@
-writefile("executed.txt","executed")
+local Players = game:GetService("Players")
+repeat wait() until game.JobId ~= nil
+local username = Players.LocalPlayer.Name 
+
+writefile(username .. ".txt","executed")
